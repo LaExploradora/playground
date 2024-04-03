317266103ba6c06189633216a949cae7ba176209
Dmitry Nikulin <russiankenny@gmail.com>
2024-04-03 23:41:33 +0300

    feat: intro card shenanigans

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,11 +1,23 @@
+import { MouseEvent } from 'react';
+
 import { Layout } from '@components/Layout';
 
 import styles from './IndexPage.module.scss';
 
 export const IndexPage = () => {
+  const handleMouseMove = (e: MouseEvent<HTMLElement>) => {
+    const { currentTarget: target } = e;
+    const rect = target.getBoundingClientRect(),
+      x = e.clientX - rect.left,
+      y = e.clientY - rect.top;
+
+    target.style.setProperty('--mouse-x', `${x}px`);
+    target.style.setProperty('--mouse-y', `${y}px`);
+  };
+
   return (
     <Layout>
-      <div className={styles.introCard}>
+      <div className={styles.introCard} onMouseMove={handleMouseMove}>
         <div className={styles.infoContainer}>
           <h1 className={styles.title}>LaExploradora.</h1>
           <p className={styles.subtitle}>Dmitry Nikulin, frontend developer.</p>
